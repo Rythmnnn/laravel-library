20ae82b31a247b833d8659c060877dde7fa73755
Rep. <agungwibawaput@gmail.com>
2026-02-10 13:29:12 +0700

    baru

--- app/Http/Controllers/DendaController.php
+++ app/Http/Controllers/DendaController.php
@@ -3,8 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Midtrans\Config;
+use Midtrans\Snap;
 
 class DendaController extends Controller
 {
-    //
+    public function bayar(Request $request)
+    {
+        Config::$serverKey = config('midtrans.server_key');
+        Config::$isProduction = config('midtrans.is_production');
+        Config::$isSanitized = config('midtrans.is_sanitized');
+        Config::$is3ds = config('midtrans.is_3ds');
+
+        $params = [
+            'transaction_details' => [
+                'order_id' => 'DENDA-' . time(),
+                'gross_amount' => $request->jumlah_denda,
+            ],
+            'customer_details' => [
+                'first_name' => $request->nama,
+                'email' => $request->email,
+            ],
+        ];
+
+        $snapToken = Snap::getSnapToken($params);
+
+        return view('denda.bayar', compact('snapToken'));
+    }
+
+    public function callback(Request $request)
+{
+    $serverKey = config('midtrans.server_key');
+    $signatureKey = hash('sha512',
+        $request->order_id .
+        $request->status_code .
+        $request->gross_amount .
+        $serverKey
+    );
+
+    if ($signatureKey != $request->signature_key) {
+        return response()->json(['message' => 'Invalid signature'], 403);
+    }
+
+    if ($request->transaction_status == 'settlement') {
+        // update status denda -> LUNAS
+    }
+
+    return response()->json(['message' => 'OK']);
+}
+
 }
